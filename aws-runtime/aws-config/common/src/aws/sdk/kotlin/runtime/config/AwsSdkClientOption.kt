52b39c69c7b995290f11f19eef1123805af1ee46
Matas Lauzadis <lauzmata@amazon.com>
2025-10-23 13:52:06 -0400

    Add KDocs

--- aws-runtime/aws-config/common/src/aws/sdk/kotlin/runtime/config/AwsSdkClientOption.kt
+++ aws-runtime/aws-config/common/src/aws/sdk/kotlin/runtime/config/AwsSdkClientOption.kt
@@ -7,7 +7,14 @@ package aws.sdk.kotlin.runtime.config
 
 import aws.smithy.kotlin.runtime.collections.AttributeKey
 
-// AWS-specific version of smithy-kotlin's SdkClientOption
+/**
+ * Common client execution options.
+ * AWS-specific version of [aws.smithy.kotlin.runtime.client.SdkClientOption]
+ */
 public object AwsSdkClientOption {
+    /**
+     * An optional application specific identifier.
+     * When set it will be appended to the User-Agent header of every request in the form of: `app/{applicationId}`.
+     */
     public val ApplicationId: AttributeKey<String> = AttributeKey("aws.sdk.kotlin#ApplicationId")
 }
