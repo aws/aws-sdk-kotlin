6e4c42a2966bd923e20dfb13cb066b48f34aacb3
Matas Lauzadis <lauzmata@amazon.com>
2025-10-31 15:38:19 -0400

    add ignore import

--- services/polly/common/test/aws/sdk/kotlin/services/polly/PollyTest.kt
+++ services/polly/common/test/aws/sdk/kotlin/services/polly/PollyTest.kt
@@ -16,6 +16,7 @@ import aws.smithy.kotlin.runtime.http.engine.HttpClientEngineConfig
 import aws.smithy.kotlin.runtime.http.request.HttpRequest
 import aws.smithy.kotlin.runtime.operation.ExecutionContext
 import kotlinx.coroutines.test.runTest
+import kotlin.test.Ignore
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
