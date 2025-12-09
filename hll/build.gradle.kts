64189d45c995ffddeb39f75c0160f456f8fe6d45
Matas <lauzmata@amazon.com>
2025-12-09 12:04:49 -0500

    misc: remove noisy println, outdated TODOs (#1747)

--- hll/build.gradle.kts
+++ hll/build.gradle.kts
@@ -46,7 +46,6 @@ val hllPreviewVersion = if (sdkVersion.contains("-SNAPSHOT")) { // e.g. 1.3.29-b
 subprojects {
     group = "aws.sdk.kotlin"
     version = hllPreviewVersion
-    // TODO Use configurePublishing when migrating to Sonatype Publisher API / JReleaser
     configurePublishing("aws-sdk-kotlin")
 }
 
