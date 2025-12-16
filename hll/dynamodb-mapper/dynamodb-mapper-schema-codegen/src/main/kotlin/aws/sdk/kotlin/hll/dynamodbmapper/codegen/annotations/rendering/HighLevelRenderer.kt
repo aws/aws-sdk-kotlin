21f1e399511f7336c9db9793682017efd74faaa1
Matas Lauzadis <lauzmata@amazon.com>
2025-12-16 11:37:48 -0500

    logger is already available in renderCtx

--- hll/dynamodb-mapper/dynamodb-mapper-schema-codegen/src/main/kotlin/aws/sdk/kotlin/hll/dynamodbmapper/codegen/annotations/rendering/HighLevelRenderer.kt
+++ hll/dynamodb-mapper/dynamodb-mapper-schema-codegen/src/main/kotlin/aws/sdk/kotlin/hll/dynamodbmapper/codegen/annotations/rendering/HighLevelRenderer.kt
@@ -42,7 +42,7 @@ internal class HighLevelRenderer(
                 attributes,
             )
 
-            val annotation = SchemaRenderer(logger, annotated, renderCtx)
+            val annotation = SchemaRenderer(annotated, renderCtx)
             annotation.render()
         }
     }
