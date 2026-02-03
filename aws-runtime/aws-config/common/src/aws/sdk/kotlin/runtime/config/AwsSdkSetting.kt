29387c12c07efa2b36e56211e1ebdc61b755bda4
Matas Lauzadis <lauzmata@amazon.com>
2026-02-02 19:19:58 -0500

    merge from main

--- aws-runtime/aws-config/common/src/aws/sdk/kotlin/runtime/config/AwsSdkSetting.kt
+++ aws-runtime/aws-config/common/src/aws/sdk/kotlin/runtime/config/AwsSdkSetting.kt
@@ -106,14 +106,6 @@ public object AwsSdkSetting {
     public val AwsEc2MetadataServiceEndpointMode: EnvironmentSetting<String> =
         strEnvSetting("aws.ec2MetadataServiceEndpointMode", "AWS_EC2_METADATA_SERVICE_ENDPOINT_MODE")
 
-    /**
-     * The name of the EC2
-     * [instance profile](https://docs.aws.amazon.com/IAM/latest/UserGuide/id_roles_use_switch-role-ec2_instance-profiles.html)
-     * to use for resolving credentials on an EC2 instance. Specifying this name disables profile name discovery.
-     */
-    public val AwsEc2InstanceProfileName: EnvironmentSetting<String> =
-        strEnvSetting("aws.ec2InstanceProfileName", "AWS_EC2_INSTANCE_PROFILE_NAME")
-
     // TODO Currently env/system properties around role ARN, role session name, etc are restricted to the STS web
     //  identity provider. They should be applied more broadly but this needs fleshed out across AWS SDKs before we can
     //  do so.
@@ -219,15 +211,6 @@ public object AwsSdkSetting {
      */
     public val AwsSigV4aSigningRegionSet: MultipleKeyEnvironmentSetting<String> = strEnvSetting("aws.sigV4aSigningRegionSet", "AWS_SIGV4A_SIGNING_REGION_SET")
         .withAdditionalKeys(listOf("aws.sigv4a.signing.region.set"))
-
-    /**
-     * A flag indicating whether endpoint discovery is enabled for AWS services that support it. The implicit default
-     * value for this setting is:
-     * * `true` for services which _require_ EP discovery (e.g., Timestream)
-     * * `false` for services which _allow but do not require_ EP discovery (e.g., DynamoDB)
-     */
-    public val AwsEndpointDiscoveryEnabled: EnvironmentSetting<Boolean> =
-        boolEnvSetting("aws.endpointDiscoveryEnabled", "AWS_ENABLE_ENDPOINT_DISCOVERY")
 
     /**
      * A flag indicating whether endpoint discovery is enabled for AWS services that support it. The implicit default
