933b6a579756e72b24aa4117f9a4ddc143f4cc81
Matas Lauzadis <lauzmata@amazon.com>
2026-02-09 10:24:55 -0500

    update e2e-test-util deps

--- tests/e2e-test-util/build.gradle.kts
+++ tests/e2e-test-util/build.gradle.kts
@@ -3,12 +3,26 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 plugins {
-    id(libs.plugins.kotlin.jvm.get().pluginId)
+    id(libs.plugins.kotlin.multiplatform.get().pluginId)
+    alias(libs.plugins.aws.kotlin.repo.tools.kmp)
 }
 
 description = "Test utilities for integration and e2e tests"
 
-dependencies {
-    api(libs.smithy.kotlin.http.client.engine.default)
-    api(libs.smithy.kotlin.http.client.engine.crt)
+kotlin {
+    sourceSets {
+        commonMain {
+            dependencies {
+                api(libs.smithy.kotlin.http.client.engine.crt)
+            }
+        }
+        
+        jvmMain {
+            dependencies {
+                api(libs.smithy.kotlin.http.client.engine.default)
+                implementation(libs.smithy.kotlin.http.client.engine.okhttp)
+                implementation(libs.smithy.kotlin.http.client.engine.okhttp4)
+            }
+        }
+    }
 }
