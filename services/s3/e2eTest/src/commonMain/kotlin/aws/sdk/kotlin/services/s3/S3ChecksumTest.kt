0d8c659bb98c35a3c8dce880d13efb7e3e876c0d
Matas Lauzadis <lauzmata@amazon.com>
2026-02-10 18:28:30 -0500

    refactor S3ChecksumTest

--- services/s3/e2eTest/src/commonMain/kotlin/aws/sdk/kotlin/services/s3/S3ChecksumTest.kt
+++ services/s3/e2eTest/src/commonMain/kotlin/aws/sdk/kotlin/services/s3/S3ChecksumTest.kt
@@ -2,14 +2,8 @@
  * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
-
 package aws.sdk.kotlin.e2etest
 
-import aws.sdk.kotlin.e2etest.S3TestUtils.deleteBucketContents
-import aws.sdk.kotlin.e2etest.S3TestUtils.deleteMultiPartUploads
-import aws.sdk.kotlin.e2etest.S3TestUtils.getAccountId
-import aws.sdk.kotlin.e2etest.S3TestUtils.getTestBucket
-import aws.sdk.kotlin.e2etest.S3TestUtils.responseCodeFromPut
 import aws.sdk.kotlin.services.s3.*
 import aws.sdk.kotlin.services.s3.model.*
 import aws.sdk.kotlin.services.s3.presigners.presignPutObject
@@ -17,36 +11,27 @@ import aws.smithy.kotlin.runtime.content.*
 import aws.smithy.kotlin.runtime.hashing.crc32
 import aws.smithy.kotlin.runtime.testing.RandomTempFile
 import kotlinx.coroutines.runBlocking
-import org.junit.jupiter.api.*
-import java.io.File
-import java.io.FileInputStream
-import java.util.*
-import kotlin.test.assertEquals
-import kotlin.test.assertFalse
-import kotlin.test.assertTrue
+import kotlin.random.Random
+import kotlin.test.*
 import kotlin.time.Duration.Companion.seconds
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class S3ChecksumTest {
     private val client = S3Client { region = "us-west-2" }
     private lateinit var testBucket: String
-    private fun testKey(): String = "test-object" + UUID.randomUUID()
+    private fun testKey(): String = "test-object${Random.nextInt()}"
 
-    @BeforeAll
-    private fun setUp(): Unit = runBlocking {
-        val accountId = getAccountId()
-        testBucket = getTestBucket(client, "us-west-2", accountId)
+    @BeforeTest
+    fun setUp() = runBlocking {
+        testBucket = S3TestUtils.getOrCreateSharedBucket(client, "us-west-2")
     }
 
-    @AfterAll
-    private fun cleanUp(): Unit = runBlocking {
-        deleteMultiPartUploads(client, testBucket)
-        deleteBucketContents(client, testBucket)
-        client.close()
+    @AfterTest
+    fun cleanUp() = runBlocking {
+        S3TestUtils.cleanupSharedBucket(client)
     }
 
     @Test
-    fun testPutObject(): Unit = runBlocking {
+    fun testPutObject() = runBlocking {
         val testBody = "Hello World"
         val testKey = testKey()
 
@@ -67,7 +52,7 @@ class S3ChecksumTest {
     }
 
     @Test
-    fun testPutObjectWithEmptyBody(): Unit = runBlocking {
+    fun testPutObjectWithEmptyBody() = runBlocking {
         val testKey = testKey()
         val testBody = ""
 
@@ -86,21 +71,16 @@ class S3ChecksumTest {
         }
     }
 
+    // TODO This test is not actually aws-chunked encoded?
     @Test
-    fun testPutObjectAwsChunkedEncoded(): Unit = runBlocking {
+    fun testPutObjectAwsChunkedEncoded() = runBlocking {
         val testKey = testKey()
         val testBody = "Hello World"
-
-        val tempFile = File.createTempFile("test", ".txt").also {
-            it.writeText(testBody)
-            it.deleteOnExit()
-        }
-        val inputStream = FileInputStream(tempFile)
 
         client.putObject {
             bucket = testBucket
             key = testKey
-            body = ByteStream.fromInputStream(inputStream, testBody.length.toLong())
+            body = ByteStream.fromString(testBody)
         }
 
         client.getObject(
@@ -114,11 +94,10 @@ class S3ChecksumTest {
     }
 
     @Test
-    fun testMultiPartUpload(): Unit = runBlocking {
+    fun testMultiPartUpload() = runBlocking<Unit> {
         val testKey = testKey()
-
-        val partSize = 5 * 1024 * 1024 // 5 MB - min part size
-        val contentSize: Long = 8 * 1024 * 1024 // 2 parts
+        val partSize = 5 * 1024 * 1024
+        val contentSize: Long = 8 * 1024 * 1024
         val file = RandomTempFile(sizeInBytes = contentSize)
 
         val expectedChecksum = file.readBytes().crc32()
@@ -128,24 +107,24 @@ class S3ChecksumTest {
             key = testKey
         }.uploadId
 
-        val uploadedParts = file.chunk(partSize).mapIndexed { index, chunk ->
-            val adjustedIndex = index + 1 // index starts from 0 but partNumber needs to start from 1
+        val fileBytes = file.readBytes()
+        val chunks = fileBytes.chunk(partSize).toList()
+        val uploadedParts = chunks.mapIndexed { index, chunk ->
+            val adjustedIndex = index + 1
 
-            runBlocking {
-                client.uploadPart {
-                    bucket = testBucket
-                    key = testKey
+            client.uploadPart {
+                bucket = testBucket
+                key = testKey
+                partNumber = adjustedIndex
+                uploadId = testUploadId
+                body = ByteStream.fromBytes(chunk)
+            }.let {
+                CompletedPart {
                     partNumber = adjustedIndex
-                    uploadId = testUploadId
-                    body = file.asByteStream(chunk)
-                }.let {
-                    CompletedPart {
-                        partNumber = adjustedIndex
-                        eTag = it.eTag
-                    }
+                    eTag = it.eTag
                 }
             }
-        }.toList()
+        }
 
         client.completeMultipartUpload {
             bucket = testBucket
@@ -165,7 +144,10 @@ class S3ChecksumTest {
             val actualChecksum = actual.body!!.toByteArray().crc32()
             assertEquals(actualChecksum, expectedChecksum)
         }
+
+        file.delete()
     }
+
 
     @Test
     fun testPresignedUrlNoDefault() = runBlocking {
@@ -178,7 +160,7 @@ class S3ChecksumTest {
         val presignedPutRequest = client.presignPutObject(unsignedPutRequest, 60.seconds)
 
         assertFalse(presignedPutRequest.url.toString().contains("x-amz-checksum-crc32"))
-        assertTrue(responseCodeFromPut(presignedPutRequest, contents) in 200..299)
+        assertTrue(responseCodeFromPut(client.config.httpClient, presignedPutRequest, contents) in 200..299)
     }
 
     @Test
@@ -193,6 +175,6 @@ class S3ChecksumTest {
         val presignedPutRequest = client.presignPutObject(unsignedPutRequest, 60.seconds)
 
         assertTrue(presignedPutRequest.url.toString().contains("x-amz-checksum-crc32"))
-        assertTrue(responseCodeFromPut(presignedPutRequest, contents) in 200..299)
+        assertTrue(responseCodeFromPut(client.config.httpClient, presignedPutRequest, contents) in 200..299)
     }
 }
