7a964581f472b954b3aaa5c1527c9bba0f1cae85
Ian Botsford <83236726+ianbotsf@users.noreply.github.com>
2026-02-17 23:57:01 +0000

    fix: remove explicit Gradle dependencies on kotlin-stdlib in service clients

---
+++ codegen/aws-sdk-codegen/src/test/kotlin/aws/sdk/kotlin/codegen/GradleGeneratorTest.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package aws.sdk.kotlin.codegen
+
+import aws.sdk.kotlin.codegen.testutil.model
+import aws.smithy.kotlin.codegen.core.KotlinDependency
+import aws.smithy.kotlin.codegen.core.RUNTIME_VERSION
+import aws.smithy.kotlin.codegen.rendering.writeGradleBuild
+import aws.smithy.kotlin.codegen.test.defaultSettings
+import aws.smithy.kotlin.codegen.test.shouldContainOnlyOnceWithDiff
+import aws.smithy.kotlin.codegen.test.shouldNotContainWithDiff
+import org.junit.jupiter.api.Test
+import software.amazon.smithy.build.MockManifest
+
+class GradleGeneratorTest {
+    @Test
+    fun testRenderDependencies() {
+        val model = model()
+        val settings = model.defaultSettings()
+        val manifest = MockManifest()
+        val dependencies = listOf(KotlinDependency.KOTLIN_STDLIB, KotlinDependency.CORE)
+        writeGradleBuild(settings, manifest, dependencies)
+
+        val contents = manifest.getFileString("build.gradle.kts").get()
+
+        contents.shouldContainOnlyOnceWithDiff("""api("aws.smithy.kotlin:runtime-core:$RUNTIME_VERSION")""")
+        contents.shouldNotContainWithDiff("stdlib") // stdlib dependencies are implicit
+    }
+}
